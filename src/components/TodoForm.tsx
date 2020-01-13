d79c554012abc1e32da91f9f37568fe56f3ff553
Слепнев Александр Юрьевич <ayslepnev@domclick.ru>
2020-01-13 09:15:46 +0300

    TYPESCRIPT: types

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,16 +1,49 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 interface OwnProps {
+  onAdd(title: string): void
 }
 
 type Props = OwnProps;
 
-const TodoForm: React.FC<Props> = () => {
-  const [title, setTitle] = useState('');
+const TodoForm: React.FC<Props> = props => {
+  /* Реализация через State */
+  // const [title, setTitle] = useState<string>('');
+
+  // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTitle(event.target.value);
+  // };
+
+  // const keyPressHandler = (event: React.KeyboardEvent) => {
+  //   if (event.key === 'Enter') {
+  //     console.log(title);
+  //     setTitle('');
+  //   }
+  // };
+
+  /* Реализация через Ref */
+  const ref = useRef<HTMLInputElement>(null);
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      props.onAdd(ref.current!.value);
+      ref.current!.value = '';
+    }
+  };
 
   return (
     <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Введите название дела"/>
+      <input
+        // onChange={changeHandler}
+        // value={title}
+
+        ref={ref}
+
+        type="text"
+        id="title"
+        placeholder="Введите название дела"
+        onKeyPress={keyPressHandler}
+      />
       <label htmlFor="title" className="active">Введите название дела</label>
     </div>
   );
