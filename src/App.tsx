39c89bb8dce232a5ee6f978792ea69fd671ece06
Слепнев Александр Юрьевич <ayslepnev@domclick.ru>
2020-01-17 09:05:59 +0300

    TYPESCRIPT: todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,21 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
-import { ITodo } from './interfaces';
-
-declare var confirm: (question: string) => boolean;
+import { TodosPage } from './pages/TodosPage';
+import { AboutPage } from './pages/AboutPage';
 
 const App: React.FunctionComponent = () => {
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  useEffect(() => {
-    const saved = localStorage.getItem('todos')
-  }, [])
-
-  const addHandler = (title: string) => {
-    const newTodo: ITodo = {
-      title,
-      id: Date.now(),
-      completed: false
-    };
-
-    setTodos(prev => [newTodo, ...prev]);
-  };
-
-  const toggleHandler = (id: number) => {
-    setTodos(prev => prev.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      console.log(todo.completed);
-
-      return todo;
-    }));
-  };
-
-  const removeHandler = (id: number) => {
-    const shouldRemove = confirm('Вы уверены что хотите удалить?');
-    if (shouldRemove) {
-      setTodos(prev => prev.filter(todo => todo.id !== id));
-    }
-  };
 
   return (
-    <Fragment>
-      <Navbar/>
+    <BrowserRouter>
+      <Navbar />
       <div className='container'>
-        <TodoForm onAdd={addHandler}/>
-
-        <TodoList todos={todos} onToggle={toggleHandler} onRemove={removeHandler}/>
+        <Switch>
+          <Route component={TodosPage} path="/" exact />
+          <Route component={AboutPage} path="/about" />
+        </Switch>
       </div>
-    </Fragment>
+    </BrowserRouter>
   );
 };
 
